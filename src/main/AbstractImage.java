32addffda7d1fa6681868f5b81aab49948af2bb0
Hunter Black <huntergblack@gmail.com>
2018-04-05 22:32:52 -0500

    Hunter's branch created with semi working app

---
+++ src/main/AbstractImage.java
@@ -0,0 +1,22 @@
+import java.awt.image.BufferedImage;
+
+/**
+ * A class to hold the images of current and previous iterations.
+ * Has an img stored, as well as a grayscale variable saying if that image is in grayscale.
+ * @author Evan
+ *
+ */
+public class AbstractImage {
+	private BufferedImage img;
+	private boolean inGrayscale;
+	
+	public AbstractImage(BufferedImage img, boolean inGrayscale) {
+		this.img = img;
+		this.inGrayscale = inGrayscale;
+	}
+	
+	public BufferedImage getImg() { return img; }
+	public boolean isInGrayScale() { return inGrayscale; }
+	public void setImg(BufferedImage img) { this.img = img; }
+	public void setInGrayScale(boolean inGrayscale) { this.inGrayscale = inGrayscale; }
+}
---
+++ src/main/Model.java
@@ -0,0 +1,42 @@
+import java.util.ArrayList;
+
+/**
+ * The <CODE>Abstractanator Model</CODE> class.<P>
+ *
+ * @author  Evan Johnston
+ * @version %I%, %G%
+ */
+
+public class Model
+{
+  /****************************************
+  * Public class variables
+  ****************************************/
+
+  /****************************************
+  * Private class variables
+  ****************************************/
+
+  private ArrayList<AbstractImage> historyList;
+
+  /****************************************
+  * Constructor(s)
+  ****************************************/
+
+  public Model()
+  {
+    historyList = new ArrayList<AbstractImage>();
+  }
+
+  /****************************************
+  * Getters and Setters
+  ****************************************/
+
+  /****************************************
+  * Public method(s)
+  ****************************************/
+
+  /****************************************
+  * Private method(s)
+  ****************************************/
+}
